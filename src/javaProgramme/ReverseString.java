4917c445eba556d1db95f2d184816a2b29f1def1
vikykham <vikas1khamat@gmail.com>
2025-08-24 17:16:58 +0530

    added reverse string program

---
+++ src/javaProgramme/ReverseString.java
@@ -0,0 +1,19 @@
+package javaProgramme;
+
+import java.util.Scanner;
+
+public class ReverseString {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter any name to reverse it");
+		String name = sc.nextLine();
+		String rev = "";
+		
+		for(int i = name.length()-1; i>=0 ; i--) {
+			rev = rev + name.charAt(i);
+		}
+		
+		System.out.println("Reverse String : "+rev);
+	}
+}
